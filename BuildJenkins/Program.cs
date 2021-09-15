7d2682099211b9cccf36c2fcb52e484a45db7c17
a36664 Nguyễn Văn Lực 😀 <a36664@thanglong.edu.vn>
2021-09-15 17:42:42 +0700

    Add project files.

---
+++ BuildJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
